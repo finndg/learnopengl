a7d77f8e926b0eb65a659ce2a2f14208ab24bf05
Finn de Graaf <finndg@outlook.com>
2020-08-23 11:21:52 +0200

    first commit

---
+++ learnopengl/shaders/f_shader.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform float mixValue;
+
+uniform sampler2D texture1, texture2;
+
+void main()
+{
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue);
+}
